c619c4e897c827d447b1f4e390561d10f211d3d6
Craig Oliver <craig.robert.oliver@gmail.com>
2025-04-14 13:21:34 -0400

    Documentation

--- mermaid_graph_from_lists.js
+++ mermaid_graph_from_lists.js
@@ -1,4 +1,10 @@
-//console.log("==== NEW RUN ====");
+/* 
+ * This script generates a mermaid graph from the list items in the current note.
+ * Each section of the note is represented as a separate branch in the graph.
+ * The nodes are connected in the order they appear in the list.  If two nodes
+ * have the same text, they are treated as the same node, allowing branches to
+ * connect to each other.
+ */
 
 /* Regular expression to filter list items.  Only list items whose text
  * matches this regex will be in the graph. */
@@ -46,9 +52,10 @@ function djb2Hash(str) {
   return (hash >>> 0).toString(16); // Unsigned 32-bit hex string
 }
 
+/* Organize the list items into sections and generate hashes for each item.  The
+ * hash is used to create a unique identifier for each node in the graph. */
 let nodes = [];
 let nodes_by_section_name = {};
-
 let page = dv.current();
 let lists = page.file.lists;
 for (const item of lists) {
@@ -73,24 +80,30 @@ for (const item of lists) {
 	nodes_by_section_name[node.section].push(node);
 }
 
+/* Genrate a mermaid graph from the nodes.  Each section is represented as a
+ * separate branch in the graph.  The nodes are connected in the order they
+ * appear in the list.  If two nodes have the same text, they are treated as the
+ * same node, allowing branches to connect to each other. */
 let output = "\n\n```mermaid\n";
-let node_color = 0;
+let branch_color = 0;
 output += "graph TD\n"
 for (const section in nodes_by_section_name) {
     let section_hash = "s" + djb2Hash(section);
     let previous_hash = section_hash;
     output += `\n  ${section_hash}["**${section}**"]\n`;
-    output += `    style ${section_hash} stroke:#000,stroke-width:3px,fill:${branch_colors[node_color]}\n`
+    output += `    style ${section_hash} stroke:#000,stroke-width:3px,fill:${branch_colors[branch_color]}\n`
     for (const node of nodes_by_section_name[section]) {
         output += `    ${node.hash}["${node.text}"]\n`;
         output += `    ${previous_hash} --> ${node.hash}\n`;
-        output += `    style ${node.hash} stroke:#000,fill:${branch_colors[node_color]}\n`
+        output += `    style ${node.hash} stroke:#000,fill:${branch_colors[branch_color]}\n`
         previous_hash = node.hash;
     }
-    node_color += 1;
-    if (node_color >= branch_colors.length) {
-        node_color = 0;
+    branch_color += 1;
+    if (branch_color >= branch_colors.length) {
+        branch_color = 0;
     }
 }
 output += "```\n\n"
+
+/* Output the graph.  The graph is displayed as a mermaid diagram in Obsidian. */
 dv.paragraph(output);
