1c8634a09139497ff0f1c49aef205fb0fc84f034
Craig Oliver <craig.robert.oliver@gmail.com>
2025-06-05 09:15:22 -0400

    topic_footer.js: Split out journal entries

--- topic_footer.js
+++ topic_footer.js
@@ -4,16 +4,23 @@ const current = dv.current();
 // Get incoming links not already linked from this page
 const inlinks = dv.pages("[[]] AND !outgoing([[]])").sort(page => page.file.name);
 
-// Get subtopics
+// Get journal entries (backlinks that start with a year)
+const journalEntries = inlinks.filter(childPage =>{
+    return childPage.file.name.match(/^\d{4}/);
+}).sort(page => page.file.name, "desc");
+
+// Get subtopics (non-journal links that are topics of the current page)
 const subtopics = inlinks.filter(childPage =>{
     return childPage.topics && 
            Array.isArray(childPage.topics) && 
-           childPage.topics.some(topic => topic.path == current.file.path);
+           childPage.topics.some(topic => topic.path == current.file.path) &&
+           journalEntries.includes(childPage) === false;
 });
 
-// Get backlinks (that is, non-subtopic links)
+// Get backlinks (non-subtopic, non-journal links)
 const backlinks = inlinks.filter(childPage =>{
-    return subtopics.includes(childPage) === false;
+    return subtopics.includes(childPage) === false &&
+           journalEntries.includes(childPage) === false;
 });
 
 // Display subtopics, if any
@@ -22,10 +29,16 @@ if (subtopics.length > 0) {
     dv.list(subtopics.file.link);
 }
 
-// Display other links, if any
+// Display backlinks, if any
 if (backlinks.length > 0) {
     dv.header(1, "Backlinks");
     dv.list(backlinks.file.link);
+}
+
+// Display journal entries, if any
+if (journalEntries.length > 0) {
+    dv.header(1, "Journal Entries");
+    dv.list(journalEntries.file.link);
 }
 
 // Display this page's topics
