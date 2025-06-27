cb92a2209acdf3ea725efd6085a41f5e2b17350f
Craig Oliver <craig.robert.oliver@gmail.com>
2025-06-27 10:04:02 -0400

    Linting

--- toc.js
+++ toc.js
@@ -1,23 +1,23 @@
 /* global dv, input */
 
 /*
- * This DataviewJS script creates a table of contents for a page or set of
- * pages, optionally filtering to just the headings that match a regex.  
- */
+* This DataviewJS script creates a table of contents for a page or set of
+* pages, optionally filtering to just the headings that match a regex.  
+*/
 
 /* Default filter regex is case insensitive */
-let filterRegexOptions = "i";
+let filterRegexOptions = 'i';
 
 /* INPUT: Pages to search, using DataView's FROM syntax. */
 let from = '"' + dv.current().file.path + '"';
-if (input && "from" in input) {
+if (input && 'from' in input) {
     from = input.from;
 }
 
 /* INPUT: Make filter case sensitive.
  * NOTE: This check has to happen before the 'regex' step because it uses
  * filterRegexOptions. */
-if (input && "caseSensitive" in input) {
+if (input && 'caseSensitive' in input) {
     if (input.caseSensitive) {
         filterRegexOptions = filterRegexOptions.replace(/i/, '');
     }
@@ -25,71 +25,70 @@ if (input && "caseSensitive" in input) {
 
 /* INPUT: Regex for filtering which headings to show */
 let filterRegex = /.*/;
-if (input && "regex" in input) {
+if (input && 'regex' in input) {
     filterRegex = new RegExp(input.regex, filterRegexOptions);
 } 
 
 /* INPUT: Search content too, not just headers */
 let searchContent = false;
-if (input && "searchContent" in input) {
+if (input && 'searchContent' in input) {
     searchContent = input.searchContent;
 } 
 
 /* INPUT: Show matching content */
 let showContent = false;
-if (input && "showContent" in input) {
+if (input && 'showContent' in input) {
     showContent = input.showContent;
 } 
 
 /* INPUT: Highlight matching content */
 let highlightContent = true;
-if (input && "highlightContent" in input) {
+if (input && 'highlightContent' in input) {
     highlightContent = input.highlightContent;
 } 
 
 /* INPUT: Maximum heading level to show. */
 let maxHeadingLevel = 6;
-if (input && "maxLevel" in input) {
+if (input && 'maxLevel' in input) {
     maxHeadingLevel = input.maxLevel;
 } 
 
 /* INPUT: Show full file paths. */
 let showFilePath = false;
-if (input && "showFilePath" in input) {
+if (input && 'showFilePath' in input) {
     showFilePath = input.showFilePath;
 } 
 
 /* INPUT: debug for debugging */
 let debug = false;
-if (input && "debug" in input) {
+if (input && 'debug' in input) {
     debug = input.debug;
 } 
 
 /* Measure elapsed time if showing debug info. */
 if (debug) {
-    console.time("filtered_toc.js Elapsed time");
+    console.time('filtered_toc.js Elapsed time');
 }
 
 /* Get pages. */
 let pages = dv.pages(from).sort(page => page.file.path);
 
 /* Abort if no pages found. */
-if (pages.length == 0) {
-    const errorMessage = `ERROR: Could not find pages for ${from}`
+if (pages.length === 0) {
+    const errorMessage = `ERROR: Could not find pages for ${from}`;
     console.error(errorMessage);
     dv.paragraph(errorMessage);
-    return
 }
 
 /* If we have only one page, don't show pages in the outline. */
 let levelOffset = 0;
 const showPages = pages.length > 1;
-if (showPages == false) {
+if (showPages === false) {
     levelOffset = -1;
 }
 
 /* For each page... */
-let out = "";
+let out = '';
 const headerRegex = /^(#{1,6})\s+(.+)/;
 for (const page of pages) {
 
@@ -104,14 +103,14 @@ for (const page of pages) {
         /* Show file path */
         if (page.file.folder) {
             /* File is in a folder: print folder and separator */
-            currentHeaders[0] = `[[${page.file.path}|${page.file.folder}/${page.file.name}]]`
+            currentHeaders[0] = `[[${page.file.path}|${page.file.folder}/${page.file.name}]]`;
         } else {
             /* File is in root: print just the name */
-            currentHeaders[0] = `[[${page.file.path}|${page.file.name}]]`
+            currentHeaders[0] = `[[${page.file.path}|${page.file.name}]]`;
         }
     } else {
         /* Don't show file paths, just the name */
-        currentHeaders[0] = `[[${page.file.path}|${page.file.name}]]`
+        currentHeaders[0] = `[[${page.file.path}|${page.file.name}]]`;
     }
 
     /* Load content of page */
@@ -121,14 +120,14 @@ for (const page of pages) {
     content.split(/\r?\n/).forEach(line => {
 
         /* Handle code blocks. */
-        if (line.startsWith("```")) {
+        if (line.startsWith('```')) {
             isInsideCodeBlock = !isInsideCodeBlock;
         }
 
         /* Check to see if this is a heading */
         let isHeading = false;
         let match = headerRegex.exec(line);
-        if (match !== null && isInsideCodeBlock == false) {
+        if (match !== null && isInsideCodeBlock === false) {
 
             /* Found a header. */
             const level = match[1].length;
@@ -140,10 +139,10 @@ for (const page of pages) {
              * match, etc., so that we maintain the full context for headers we
              * *do* want to print. */
             for (let i = 1; i <= 6; i++) {
-                if (i == level) {
+                if (i === level) {
                     currentHeaders[i] = text;
                 } else if (i > level) {
-                    currentHeaders[i] = "";
+                    currentHeaders[i] = '';
                 }
             }
             currentHeadingLevel = level;
@@ -164,12 +163,12 @@ for (const page of pages) {
 
         /* Skip if this line isn't a heading, unless we're searching
          * contents too. */
-        if (isHeading == false && searchContent == false) {
+        if (isHeading === false && searchContent === false) {
             return;
         }
 
         /* Skip if this line doesn't match the filter. */
-        if (matchesFilter == false) {
+        if (matchesFilter === false) {
             return;
         }
 
@@ -177,30 +176,30 @@ for (const page of pages) {
          * this will not re-print the same heading twice in a row even if
          * called multiple times. */
         for (let i = 0; i <= 6; i++) {
-            if (i <= currentHeadingLevel && printedHeaders[i] != currentHeaders[i]) {
-                if (i == 0) {
+            if (i <= currentHeadingLevel && printedHeaders[i] !== currentHeaders[i]) {
+                if (i === 0) {
                     if (showPages) {
                         out += `- ${currentHeaders[0]}\n`;
                     }
                 } else {
-                    const indent = "  ".repeat(i + levelOffset);
+                    const indent = '  '.repeat(i + levelOffset);
                     out += `${indent}- [[${page.file.path}#${currentHeaders[i]}|${currentHeaders[i]}]]\n`;
                 }
             }
         }
 
         /* Print content if requested */
-        if (isHeading == false && showContent) {
-            const indent = "  ".repeat(currentHeadingLevel + levelOffset + 1);
+        if (isHeading === false && showContent) {
+            const indent = '  '.repeat(currentHeadingLevel + levelOffset + 1);
             let displayLine = line.trim();
-            displayLine = displayLine.replace(/^- /, "");
-            displayLine = displayLine.replace(/^\d+\. /, "");
+            displayLine = displayLine.replace(/^- /, '');
+            displayLine = displayLine.replace(/^\d+\. /, '');
             if (highlightContent) {
                 match = filterRegex.exec(displayLine);
                 displayLine = displayLine.substring(0, match.index)
-                    + "=="
+                    + '=='
                     + displayLine.substring(match.index, match.index + match[0].length)
-                    + "=="
+                    + '=='
                     + displayLine.substring(match.index + match[0].length);
             }
             out += `${indent}- ${displayLine}\n`;
@@ -214,17 +213,17 @@ for (const page of pages) {
 
 /* Debug output if requested */
 if (debug) {
-    out = "```\n" 
-      + "This is debug output.  Set 'debug' to false to see normal output.\n"
-      + "Elapsed time is printed to the console.\n\n"
+    out = '```\n' 
+      + 'This is debug output.  Set `debug` to false to see normal output.\n'
+      + 'Elapsed time is printed to the console.\n\n'
       + `from: ${from}\n`
       + `pages: ${pages.file.path}\n`
       + `filterRegex: ${filterRegex}\n`
       + `input: ${JSON.stringify(input)}\n`
-      + "\n"
+      + '\n'
       + out 
-      + "\n```";
-    console.timeEnd("filtered_toc.js Elapsed time");
+      + '\n```';
+    console.timeEnd('filtered_toc.js Elapsed time');
 }
 
 /* Return Markdown text. */
