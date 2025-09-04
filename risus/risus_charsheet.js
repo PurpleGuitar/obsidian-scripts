29f358e7aa0f2723b8d77b59c15b06cf077c416b
Craig Oliver <craig.robert.oliver@gmail.com>
2025-09-04 19:46:21 -0400

    Added new RISUS charsheet

---
+++ risus/risus_charsheet.js
@@ -0,0 +1,11 @@
+let current = dv.current();
+
+if (current.cliches) {
+    dv.el("h1", "Clichés");
+    dv.list(current.cliches);
+}
+
+if (current.extras) {
+    dv.el("h1", "Extras");
+    dv.list(current.extras);
+}
